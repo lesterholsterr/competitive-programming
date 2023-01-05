644e6e1493d151c936925c3c41109fd066a18c05
lesterholsterr <mattyang98@gmail.com>
2023-01-04 19:45:16 -0500

    01/04/2023

--- LeetCode/Python/Array/two_sum.py
+++ LeetCode/Python/Array/two_sum.py
@@ -5,8 +5,18 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # Solution 1: O(n^2)
         for i in range(len(nums)):
             x = target - nums[i]
             for j in range(i+1, len(nums)):
                 if nums[j] == x:
                     return [i, j]
+
+        # Solution 2: O(n)
+        vals = {}
+        for i in range(len(nums)):
+            x = target - nums[i]
+            if x in vals:
+                return [i, vals[x]]
+            else:
+                vals[nums[i]] = i
