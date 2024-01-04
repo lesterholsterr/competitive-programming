81ae11e798c6351527f8bddecf4d6aaccb0b837a
lesterholsterr <mattyang98@gmail.com>
2024-01-03 20:52:07 -0500

    subsets

--- LeetCode/Neetcode/Backtracking/subsets.py
+++ LeetCode/Neetcode/Backtracking/subsets.py
@@ -1,5 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        # Overall: Had the right idea but could not get the recursion correct
+        # Leaps
+        # - How can we ensure no elements are repeated? (create the binary tree)
+        # - Realize the leaves of the binary tree are exactly what we want to return, so we should
+        #   only append to res in the base case
+
         res = []
 
         subset = []
