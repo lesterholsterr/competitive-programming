59f952510c561ee998d75257b3e139602a65c1ae
lesterholsterr <mattyang98@gmail.com>
2024-02-28 23:59:21 -0500

    kth largest element in a stream

--- LeetCode/Neetcode150/Heap & Priority Queue/Kth_largest_element_in_stream.py
+++ LeetCode/Neetcode150/Heap & Priority Queue/Kth_largest_element_in_stream.py
@@ -1,22 +1,18 @@
+# Overall: In hindsight I see how the heap data structure is useful. Min heap lets us repeatedly insert
+# elements and remove the smallest element in log time (as opposed to linear for lists)
+
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        nums.sort()
         self.k = k
-        self.stream = deque(nums)
+        self.minHeap = nums
+        heapq.heapify(self.minHeap)  # O(logn)
+
+        while len(self.minHeap) > k:  # Worst case runs n times
+            heapq.heappop(self.minHeap)  # O(logn)
 
     def add(self, val: int) -> int:
-        if len(self.stream) == 0:
-            self.stream.append(val)
-        elif val >= self.stream[-1]:
-            self.stream.append(val)
-        else:
-            for i in range(len(self.stream)):
-                if val <= self.stream[i]:
-                    self.stream.insert(i, val)
-        return self.stream[-(self.k)]
-
-
-# Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)
-# param_1 = obj.add(val)
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
